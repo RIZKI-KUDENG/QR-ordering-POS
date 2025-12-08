82b3fedd5ebbcab5d501b85d013f6d5ce3349f5f
M Rizki Arya Nanda <rizkiarya0611@gmail.com>
2025-12-08 22:43:01 +0700

    setup FE

--- server/src/routes/payment.js
+++ server/src/routes/payment.js
@@ -1,9 +1,9 @@
 import express from "express";
 
-import {MidtransWebhook} from "../api/payment/webhook.js";
+import {midtransWebhook} from "../api/payment/webhook.js";
 
 const paymentRouter = express.Router();
 
-paymentRouter.post('/notification', MidtransWebhook);
+paymentRouter.post('/notification', midtransWebhook);
 
 export default paymentRouter;
