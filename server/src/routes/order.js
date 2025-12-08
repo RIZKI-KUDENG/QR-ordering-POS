075afc7616e8b7d745f71aeaa1aabbcbb37c515b
M Rizki Arya Nanda <rizkiarya0611@gmail.com>
2025-12-08 22:31:14 +0700

    middleware order

--- server/src/routes/order.js
+++ server/src/routes/order.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { createOrder, getOrders, updateOrders } from '../controllers/orderControllers.js';
+import { verifyKitchen, authMiddleware } from '../middleware/authMiddleware.js';
 
 const orderRouter = express.Router();
-
 orderRouter.post('/', createOrder);
+
+orderRouter.use(authMiddleware);
+orderRouter.use(verifyKitchen);
+
 orderRouter.get('/kitchen', getOrders);
 orderRouter.patch('/kitchen/:id/status', updateOrders);
 
