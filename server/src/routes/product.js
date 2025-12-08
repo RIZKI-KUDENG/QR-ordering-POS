383e522b1dcd33d32688948309e1eac17fd8fcaa
M Rizki Arya Nanda <rizkiarya0611@gmail.com>
2025-12-08 22:30:48 +0700

    add middleware

--- server/src/routes/product.js
+++ server/src/routes/product.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import { createProduct, getProductById, getProducts, deleteProduct, updateProduct } from '../controllers/productControllers.js';
+import { verifyAdmin, authMiddleware } from '../middleware/authMiddleware.js';
 
 const productRouter = express.Router();
+productRouter.get('/:id', getProductById);
+productRouter.get('/', getProducts);
+
+productRouter.use(authMiddleware);
+productRouter.use(verifyAdmin);
 
 productRouter.post('/', createProduct);
-productRouter.get('/', getProducts);
-productRouter.get('/:id', getProductById);
 productRouter.delete('/:id', deleteProduct);
 productRouter.put('/:id', updateProduct);
 
--- server/src/routes/table.js
+++ server/src/routes/table.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import { getTables, createTable, getTableById, updateTable, getTableByToken } from '../controllers/tableControllers.js';
+import { verifyAdmin, authMiddleware } from '../middleware/authMiddleware.js';
 
 const tableRouter = express.Router();
+tableRouter.get('/scan/:token', getTableByToken);
+
+tableRouter.use(authMiddleware);
+
 
 tableRouter.get('/', getTables);
-tableRouter.post('/', createTable);
 tableRouter.get('/:id', getTableById);
-tableRouter.put('/:id', updateTable);
-tableRouter.get('/scan/:token', getTableByToken);
+tableRouter.post('/', verifyAdmin, createTable);
+tableRouter.put('/:id', verifyAdmin, updateTable);
 
 export default tableRouter;
